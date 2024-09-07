da59a17272d24c787557a0ebfa540afcaf0bc8c7
ibrahimHuseynov7 <118387768+ibrahimHuseynov7@users.noreply.github.com>
2024-09-07 19:40:25 +0400

    Update task3.py

--- Lists/task3.py
+++ Lists/task3.py
@@ -4,10 +4,10 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8]
 
 numbers_copy = numbers.copy()
 
-# Dilimləmə ilə kopyalanan list-dən ilk 3 elementi əldə edirik
+# Dilimləmə ilə kopyalanan list-dən ilk 3 elementi əldə edirik, mən burda dərsdə kecdiyimiz slice yəni dilimləmə istifadə etmisəm
 first_three_words = numbers_copy[:3]
 
-# Nəticədə dəyişdirilmiş list-ləri ekrana çıxaraq
+# Nəticədə dəyişdirilmiş list-ləri ekrana çıxaraq 
 print("Original list:", numbers)
 print("Copied list:", numbers_copy)
 print("First three numbers:", first_three_words)
